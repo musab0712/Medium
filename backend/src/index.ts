30613e47f1fed790a4edef4c1d1941a3a2c48ca0
Musab Hassan <musabhassan1999@gmail.com>
2024-04-10 10:56:38 +0530

    add prisma

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,35 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-export default app
+app.post("/api/v1/user/signup", (c) => {
+  return c.text("helo user signup");
+});
+
+app.post("/api/v1/user/signin", (c) => {
+  return c.text("helo user signin");
+});
+
+app.post("/api/v1/blog", (c) => {
+  return c.text("helo user blog");
+});
+
+app.put("/api/v1/blog", (c) => {
+  return c.text("helo user blog");
+});
+
+app.post("/api/v1/blog/:id", (c) => {
+  const id = c.req.param("id");
+  console.log(id);
+  return c.text("helo user blog id");
+});
+
+app.get("/api/v1/blog/bulk", (c) => {
+  return c.text("helo user blog bulk");
+});
+
+export default app;
