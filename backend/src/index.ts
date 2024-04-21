27cd9af1b3cb06607d846fc9c0868feaa3518c73
Musab Hassan <musabhassan1999@gmail.com>
2024-04-21 20:20:21 +0530

    uupdate

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,35 +1,49 @@
 import { Hono } from "hono";
-import { PrismaClient } from "@prisma/client/edge";
-import { withAccelerate } from "@prisma/extension-accelerate";
-import { sign } from "hono/jwt";
 import { userRouter } from "./routes/user";
-import { blogRouter } from "./routes/blog";
 
 const app = new Hono();
 
 app.route("/api/v1/user", userRouter);
-app.route("/api/v1/blog", blogRouter);
-
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
-});
-
-app.post("/api/v1/blog", (c) => {
-  return c.text("helo user blog");
-});
-
-app.put("/api/v1/blog", (c) => {
-  return c.text("helo user blog");
-});
-
-app.post("/api/v1/blog/:id", (c) => {
-  const id = c.req.param("id");
-  console.log(id);
-  return c.text("helo user blog id");
-});
-
-app.get("/api/v1/blog/bulk", (c) => {
-  return c.text("helo user blog bulk");
-});
 
 export default app;
+// routes/blog.js
+//import { Hono } from "hono";
+// import { PrismaClient } from "@prisma/client/edge";
+// import { withAccelerate } from "@prisma/extension-accelerate";
+
+// export const blogRouter = new Hono<{
+//   Bindings: {
+//     DATABASE_URL: string;
+//     JWT_SECRET: string;
+//   };
+// }>();
+
+// blogRouter.post("/", async (c) => {
+//   const body = await c.req.json();
+//   console.log(body);
+//   const prisma = new PrismaClient({
+//     datasourceUrl: c.env?.DATABASE_URL,
+//   }).$extends(withAccelerate());
+//   const post = await prisma.post.create({
+//     data: {
+//       title: body.title,
+//       content: body.content,
+//       autherId: "1",
+//     },
+//   });
+//   return c.text(" blog created ");
+// });
+
+// blogRouter.put("/", (c) => {
+//   return c.text("helo user blog");
+// });
+
+// blogRouter.post("/:id", (c) => {
+//   const id = c.req.param("id");
+//   console.log(id);
+//   return c.text("helo user blog id");
+// });
+
+// blogRouter.get("/bulk", (c) => {
+//   return c.text("helo user blog bulk");
+// });
