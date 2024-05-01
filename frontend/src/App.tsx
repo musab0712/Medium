84041df2d92a8c340fe865bb9055d91b6193347f
Musab Hassan <musabhassan1999@gmail.com>
2024-05-01 12:02:34 +0530

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Signin } from "./pages/Signin";
+import { Signup } from "./pages/Signup";
+import { Blog } from "./pages/Blog";
+
 function App() {
-  return <> Medium Home Page</>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/blog" element={<Blog />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
